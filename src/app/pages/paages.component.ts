2fd5082e63554597c2986d240e6706b71d267750
sunilpoudel123 <sunilpoudel11@gmail.com>
2022-12-11 07:56:59 +0545

    home page initialized

---
+++ src/app/pages/paages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paages',
+  templateUrl: './paages.component.html',
+  styleUrls: ['./paages.component.css']
+})
+export class PaagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
